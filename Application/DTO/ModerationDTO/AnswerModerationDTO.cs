db8a7f611374913500844b80b848f43b232c62ce
AdamB777 <akrupa777@gmail.com>
2024-03-19 11:27:52 +0100

    moderation

---
+++ Application/DTO/ModerationDTO/AnswerModerationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.ModerationDTO
+{
+    public class AnswerModerationDTO
+    {
+        public string Token { get; set; }
+        public List<int> Answer { get; set; }
+    }
+}
---
+++ apiaidev/Controllers/ModerationController.cs
@@ -0,0 +1,63 @@
+﻿using Application.CQRS;
+using Application.CQRS.moderation;
+using Application.DTO.ModerationDTO;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace apiaidev.Controllers
+{
+    public class ModerationController : BaseApiController
+    {
+        private readonly ILogger<ModerationController> _logger;
+
+        public ModerationController(IMediator mediator, ILogger<ModerationController> logger) : base(mediator)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getanswer/{token}")]
+        public async Task<IActionResult> GetTaskAsync(string token)
+        {
+            var result = await _mediator.Send(new QuestGet.Query { Token = token });
+            return Ok(result);
+        }
+
+        [HttpPost("moderate")]
+        public async Task<IActionResult> ModerateAsync(string moderationInput)
+        {
+            var command = new SendToModerationPost.Command { Sentences = moderationInput };
+            try
+            {
+                var result = await _mediator.Send(command);
+                return Ok(result);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpPost("answermoderation")]
+        public async Task<IActionResult> ModerateAsync(AnswerModerationDTO answerModerationDTO)
+        {
+            var command = new AnswerPost.Command { answerPost = answerModerationDTO };
+            try
+            {
+                var result = await _mediator.Send(command);
+                _logger.LogInformation($"Wysyłanie odpowiedzi: {JsonConvert.SerializeObject(result)}");
+                return Ok(result);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+
+    }
